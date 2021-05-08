7969c5dca18c34eed11b2dc3faa9460a3db34aff
Jose S. Daniel <djose1164@gmail.com>
2021-05-07 20:26:19 -0400

    Not sure.

--- Note/notebook.py
+++ Note/notebook.py
@@ -1,13 +1,13 @@
 import datetime
 
-# Store the next available id for all new notes. 
+# Store the next available id for all new notes.
 last_id: int = 0
 
 
 class Note:
-    """Represent a note in the notebook.
-    """
-    def __init__(self, memo, tags = ""):
+    """Represent a note in the notebook."""
+
+    def __init__(self, memo, tags=""):
         """Initialize a note with memo and optional space-separated tags.
         Automatically set the note's creation date and a unique id.
         """
@@ -17,8 +17,7 @@ class Note:
         global last_id
         last_id += 1
         self.id = last_id
-        
-        
+
     def match(self, filter):
         """Determine if this note matches the filter
         text. Return True if it matches, False otherwise.
@@ -26,17 +25,15 @@ class Note:
         Search is case sensitive and matches both text and
         tags."""
         return filter in self.memo or filter in self.tags
-    
+
 
 class Notebook:
-    """A collection of notes that can be searched, modified, and tagged.
-    """
-    
+    """A collection of notes that can be searched, modified, and tagged."""
+
     def __init__(self):
-        """Creates our list of notes.
-        """
-        self.notes:list = []
-    
+        """Creates our list of notes."""
+        self.notes: list = []
+
     def _find_note(self, note_id: int):
         """Locate the given id.
 
@@ -50,7 +47,7 @@ class Notebook:
         for note in self.notes:
             return note if str(note.id) == str(note_id) else None
 
-    def new_note(self, memo:str, tags = ""):
+    def new_note(self, memo: str, tags=""):
         """Add a new note to our list of notes.
 
         Args:
@@ -58,7 +55,7 @@ class Notebook:
             tags (str, optional): An optional tag. Defaults to "".
         """
         self.notes.append(Note(memo, tags))
-        
+
     def modify_memo(self, note_id: str, memo: int):
         """Modify a memo with a given id.
 
@@ -71,7 +68,7 @@ class Notebook:
             note.memo = memo
             return True
         return False
-            
+
     def modify_tags(self, note_id: int, tags: str):
         """Will search till meet the given note_id to make de modification.
 
@@ -80,7 +77,7 @@ class Notebook:
             tags (str): The new tag to replace.
         """
         self._find_note(note_id).tags = tags
-        
+
     def search(self, filter: str):
         """Return all notes that match with the given filter.
 
